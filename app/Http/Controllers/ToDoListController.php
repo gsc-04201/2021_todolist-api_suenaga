85ecd4e4b0b7a6c13da37573ae4ea04c846eacae
gsc-04201 <75969709+gsc-04201@users.noreply.github.com>
2021-04-27 14:19:05 +0900

    firstcommit

---
+++ app/Http/Controllers/ToDoListController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use Illuminate\Http\Request;
+
+class ToDoListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Todolist::all();
+        return response()->json([
+            'message' => 'OK',
+            'data' => $items
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Todolist;
+        $item->list = $request->list;
+        $item->save();
+        return response()->json([
+            'message' => 'Created successfully',
+            'data' => $item
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todolist $todolist)
+    {
+        $item = Todolist::where('id', $todolist->id)->first();
+        if ($item) {
+            return response()->json([
+                'message' => 'OK',
+                'data' => $item
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todolist $todolist)
+    {
+        $item = Todolist::where('id', $todolist->id)->first();
+        $item->list = $request->list;
+        $item->save();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todolist $todolist)
+    {
+        $item = Todolist::where('id', $todolist->id)->delete();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => $item,
+            ], 404);
+        }
+    }
+}
